98094fb13449f8e17464f219ee7a538765f20d51
eaktadiur <eaktadiur@gmail.com>
2014-12-09 00:57:21 +0600

    fix nav bar, user list

--- app/views/layout/top_nav_admin.blade.php
+++ app/views/layout/top_nav_admin.blade.php
@@ -1,29 +1,36 @@
-    <div class="wrapper">
-        <div id="productSearch">
-            <a href="{{ URL::route('home') }}"><img src="{{asset("/images/m-logo.png")}}" id="headerLogo"/></a>
-           <!--  <h1>Motochanic</h1> -->
-        </div>
-        
-        <div id="topNav">
-            <div id="navLinks">
-               <div class="navLink">
-                    <a href="{{ URL::route('get-product-search') }}">Search</a>
-                </div>
-                <div class="navLink" style="float:left;">
-                    <a href="{{ URL::route('user-list') }}">User List</a>
-                </div>
-                <div class="navLink" style="float:left;">
-                    <a href="{{ URL::route('user-create') }}">New User</a>
-                </div>
-                 <div class="navLink" style="float:right;">
-                    <a href="{{ URL::route('user-sign-out') }}">Logout</a>
-                </div>
-                <div class="navLink" style="float:right;"><a href="#">
-                    @if( Auth::check() )
-                    {{ Auth::user()->email }}
-                    @endif
-                    </a> </div> 
+<nav class="navbar navbar-default" role="navigation">
+  <div class="container">
+    <!-- Brand and toggle get grouped for better mobile display -->
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <a href="{{ URL::route('home') }}" class="navbar-brand"></a>
+    </div>
 
-               
-            </div>
-        </div>
+    <!-- Collect the nav links, forms, and other content for toggling -->
+    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+      <ul class="nav navbar-nav">
+        <li><a href="{{ URL::route('get-product-search') }}">Search</a></li>
+        <li><a href="{{ URL::route('user-list') }}">User List</a></li>
+        <li><a href="{{ URL::route('user-create') }}">New User</a></li>
+        <li><a href="{{ URL::route('get-product-search') }}">Search</a></li>
+      </ul>
+      
+      <ul class="nav navbar-nav navbar-right">
+        <li class="dropdown">
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
+            @if( Auth::check() )
+            {{ Auth::user()->email }}
+            @endif <span class="caret"></span></a>
+            <ul class="dropdown-menu" role="menu">
+              <li><a href="{{ URL::route('user-sign-out') }}"> Logout</a></li>
+            </ul>
+          </li>
+        </ul>
+      </div><!-- /.navbar-collapse -->
+    </div><!-- /.container-fluid -->
+  </nav>
--- app/views/users/index.blade.php
+++ app/views/users/index.blade.php
@@ -2,49 +2,42 @@
 
 @section('body-content')
 
-<div> {{ HTML::linkRoute('user-create', 'New User', '', array('class'=> 'btn btn-primary') ) }} </div>
-	<!-- <p>{{ ($message) ? $message : ''; }}</p> -->
-	 <!-- /.panel-heading -->
-                        <div class="panel-body">
-                            <div class="table-responsive">
-                                <table class="table table-striped table-bordered table-hover" id="dataTables-example">
-                                    <thead>
-                                        <tr>
-                                            <th>Ser.</th>
-                                            <th>User Name</th>
-                                            <th>Email</th>
-                                            <th>Action</th>
-                                        </tr>
-                                    </thead>
-                                   <tbody> 
-                                   @foreach ($data as $user)
-                                    
-                                        <tr>
-                                            <td>{{$ser++ }}</td>
-                                            <td>{{ $user->username }}</td>
-                                            <td class="center">{{ $user->email }}</td>
-                                            <td class="center">
-                                                {{ HTML::linkRoute('delete-user-destroy', 'Delete', array($user->id), array('class'=> 'btn btn-danger') ) }}
-			                                     {{ HTML::linkRoute('user-get-edit', 'Edit', array($user->id), array('class'=> 'btn btn-success') ) }}</td>
-                                        </tr>
-                                        @endforeach
 
-                                        
-                                    </tbody>
-                                </table>
-                            </div>
-                            <!-- /.table-responsive -->
-                            
-                        </div>
+<!-- <p>{{ ($message) ? $message : ''; }}</p> -->
+<!-- /.panel-heading -->
+<div class="row">
+  {{ HTML::linkRoute('user-create', 'New User', '', array('class'=> 'btn btn-primary') ) }}
+  <div class="table-responsive">
+    <table class="table table-striped table-bordered table-hover" id="dataTables-example">
+        <thead>
+            <tr>
+                <th>Ser.</th>
+                <th>User Name</th>
+                <th>Email</th>
+                <th>Action</th>
+            </tr>
+        </thead>
+        <tbody> 
+           @foreach ($data as $user)
+
+           <tr>
+            <td width='80'>{{$ser++ }}</td>
+            <td>{{ $user->username }}</td>
+            <td class="center">{{ $user->email }}</td>
+            <td class="center" width='150'>
+                <a href="{{ URL::route('delete-user-destroy',  array($user->id)) }}"><i class='glyphicon glyphicon-trash'></i> Delete</a> |
+                <a href="{{ URL::route('user-get-edit',  array($user->id)) }}"><i class='glyphicon glyphicon-edit'></i> Edit</a> 
+            </td>
+        </tr>
+        @endforeach
+
+
+    </tbody>
+</table>
+</div>
+<!-- /.table-responsive -->
+
+</div>
+
 
 @stop
-<!-- DataTables JavaScript -->
-{{ HTML::script('js/plugins/dataTables/jquery.dataTables.js') }}
-{{ HTML::script('js/plugins/dataTables/dataTables.bootstrap.js') }}
-
-    <!-- Page-Level Demo Scripts - Tables - Use for reference -->
-    <script>
-    $(document).ready(function() {
-        $('#dataTables-example').dataTable();
-    });
-    </script>
